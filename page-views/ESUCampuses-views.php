c09adc6703399e46fc5563682c3b4dca91f34add
asanul <asanulasanul05@gmail.com>
2025-05-12 21:32:36 +0800

    +imgs

---
+++ page-views/ESUCampuses-views.php
@@ -0,0 +1,74 @@
+<section class="ESUCIntroCont">
+    <div class="ESUCIntro">
+        <p class="ESUCIntroTitle inter-black">EXTERNAL STUDIES UNITS</p>
+        <p class="ESUCIntroSubTitle inter-regular">Western Mindanao State University (WMSU) extends its educational services beyond its main campus through a network of External Studies Units (ESUs) strategically located across various provinces. These ESUs provide accessible higher education to students in remote and underserved areas.</p>
+    </div>
+</section>
+
+<section class="ESUCMisVisCont">
+    <div class="ESUCMisVisBackgroundBoxTop"></div>
+    <p class="ESUCMisVisBackgroundTexTop">MISSION</p>
+    <div class="ESUCMisVis">
+        <div class="ESUCMisVisBox">
+            <div class="ESUCMisVisBoxCircle">
+                <div class="ESUCMisVisBoxCircleLogo">
+                    <img class="ESUCMisVisBoxCircleImg" src="../imgs/Mission.jpg" alt="">
+                </div>
+            </div>
+            <div class="ESUCMisVisBoxMain">
+                <div class="ESUCMisVisBoxMainContent">
+                    <p class="ESUCMisVisBoxMainTitle inter-bold">MISSION</p>
+                    <p class="ESUCMisVisBoxMainText inter-extralight">To educate and produce well-trained, development-oriented, and forward-looking professional and technical manpower for the socio-economic, political, and technological development of the Philippines. The university endeavors to expand the frontiers of knowledge and its applications to society through research in technology, natural resources, and the physical and social sciences.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="ESUCMisVis">
+        <div class="ESUCMisVisBox">
+            <div class="ESUCMisVisBoxCircle">
+                <div class="ESUCMisVisBoxCircleLogo">
+                    <img class="ESUCMisVisBoxCircleImg" src="../imgs/Vision.png" alt="">
+                </div>
+            </div>
+            <div class="ESUCMisVisBoxMain">
+                <div class="ESUCMisVisBoxMainContent">
+                    <p class="ESUCMisVisBoxMainTitle inter-bold">VISION</p>
+                    <p class="ESUCMisVisBoxMainText inter-extralight">To be the Center of Excellence and leading institution in human resource development and research in the country and the ASEAN region with international recognition.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="ESUCMisVisBackgroundBoxBot"></div>
+    <p class="ESUCMisVisBackgroundTexBot">VISION</p>
+</section>
+
+<section class="ESUCTitleCont">
+    <div class="ESUCTitle">
+        <p class="ESUCTitleOutlineTextTop">EXTERNAL STUDIES UNITS</p>
+        <p class="ESUCTitleText inter-black">EXTERNAL STUDIES UNITS</p>
+        <p class="ESUCTitleOutlineTextBot">EXTERNAL STUDIES UNITS</p>
+    </div>
+</section>
+
+<section class="ESUCSchoolsCont">
+    <div class="ESUCSchools">
+        <div class="ESUCSchoolsLeft">
+            <div class="ESUCSchoolsLeftBox">
+                <div class="ESUCSchoolsImgLeft"></div>
+                <p class="ESUCSchoolsTitle inter-black">ALICIA</p>
+                <p class="ESUCSchoolsTitleOutlineLeft">ALICIA</p>
+                <p class="ESUCSchoolsSubTitle inter-extralight">Zamboanga Sibugay</p>
+                <p class="ESUCSchoolsTextLeft inter-light">Established to extend WMSU's educational reach within Zamboanga Sibugay, the Alicia ESU offers programs tailored to local community needs.</p>
+            </div>
+        </div>
+        <div class="ESUCSchoolsRight">
+            <div class="ESUCSchoolsRightBox">
+                <div class="ESUCSchoolsImgRight"></div>
+                <p class="ESUCSchoolsTitle inter-black">AURORA</p>
+                <p class="ESUCSchoolsTitleOutlineRight">AURORA</p>
+                <p class="ESUCSchoolsSubTitleRight inter-extralight">Zamboanga Del Sur</p>
+                <p class="ESUCSchoolsTextRight inter-light">The Aurora ESU was developed to provide higher education opportunities in Zamboanga del Sur, focusing on courses relevant to the region's development.</p>
+            </div>
+        </div>
+    </div>
+</section>
